7b25940c2d004c6992cb287efff0eefa69184008
xyperine <ewsannne@gmail.com>
2025-07-31 22:37:40 +0300

    Added stats localization

---
+++ Assets/Scripts/StatUI.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+
+namespace DotsKiller
+{
+    public class StatUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text valueText;
+        [SerializeField] private LocalizeStringEvent labelLse;
+
+
+        public void SetText(string labelTableEntryName, string value)
+        {
+            labelLse.SetEntry($"Stats.{labelTableEntryName}.Label");
+            valueText.text = value;
+        }
+    }
+}
--- Assets/Scripts/StatsUI.cs
+++ Assets/Scripts/StatsUI.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using DotsKiller.Utility;
-using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Tables;
 using Zenject;
 
 namespace DotsKiller
 {
     public class StatsUI : MonoBehaviour
     {
-        [SerializeField] private List<TMP_Text> ts;
+        [SerializeField] private List<StatUI> statUIs;
+        [SerializeField] private LocalizedStringTable unitsTable;
 
         private Stats _stats;
 
@@ -23,11 +25,25 @@ namespace DotsKiller
 
         private void Update()
         {
-            ts[0].text = $"Kills: {_stats.Kills}";
-            ts[1].text = $"Total Points: {_stats.TotalPoints}";
-            ts[2].text = $"Points Per Kill: {Formatting.DefaultFormat(_stats.PointsPerKill)}";
-            var playtime = _stats.TotalPlaytime;
-            ts[3].text = $"Total Playtime: {playtime:%d} days {playtime:%h} hours {playtime:%m} minutes {playtime:%s} seconds";
+            statUIs[0].SetText("Kills", _stats.Kills.ToString());
+            statUIs[1].SetText("TotalPoints", _stats.TotalPoints.ToString());
+            statUIs[2].SetText("PointsPerKill", Formatting.DefaultFormat(_stats.PointsPerKill));
+            statUIs[3].SetText("TotalPlaytime", ConstructTotalPlaytimeString());
+        }
+
+
+        private string ConstructTotalPlaytimeString()
+        {
+            TimeSpan playtime = _stats.TotalPlaytime;
+            int days = playtime.Days;
+            int hours = playtime.Hours;
+            int minutes = playtime.Minutes;
+            int seconds = playtime.Seconds;
+            StringTable t = unitsTable.GetTable();
+            return $"{t.GetEntry("Units.Days").GetLocalizedString(days)} " +
+                   $"{t.GetEntry("Units.Hours").GetLocalizedString(hours)} " +
+                   $"{t.GetEntry("Units.Minutes").GetLocalizedString(minutes)} " +
+                   $"{t.GetEntry("Units.Seconds").GetLocalizedString(seconds)}";
         }
     }
 }
