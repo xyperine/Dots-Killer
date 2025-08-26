c43f998cf7d9865e940686a3a4d86a2aefe389c1
xyperine <ewsannne@gmail.com>
2025-08-26 21:43:14 +0300

    Added toggle menu button and tabs localization

---
+++ Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace DotsKiller.UI
+{
+    public class MenuUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject tabsPanelObject;
+        [SerializeField] private GameObject pagesPanelObject;
+        [SerializeField] private bool isOpenByDefault; // debug
+
+        public bool IsOpen { get; private set; }
+
+
+        private void Awake()
+        {
+            IsOpen = isOpenByDefault;
+        }
+
+
+        private void Start()
+        {
+            tabsPanelObject.SetActive(IsOpen);
+            pagesPanelObject.SetActive(IsOpen);
+        }
+
+
+        public void Toggle()
+        {
+            IsOpen = !IsOpen;
+            
+            tabsPanelObject.SetActive(IsOpen);
+            pagesPanelObject.SetActive(IsOpen);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ToggleMenuButton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace DotsKiller.UI
+{
+    public class ToggleMenuButton : MonoBehaviour
+    {
+        [SerializeField] private MenuUI menuUI;
+        [SerializeField] private Transform closedPoint;
+        [SerializeField] private Transform openPoint;
+        [SerializeField] private Transform imageTransform;
+
+
+        private void Start()
+        {
+            if (menuUI.IsOpen)
+            {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+
+        public void Toggle()
+        {
+            menuUI.Toggle();
+
+            if (menuUI.IsOpen)
+            {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+        }
+        
+        
+        public void Open()
+        {
+            transform.SetParent(openPoint, false);
+            imageTransform.rotation = Quaternion.Euler(0f, -180f, 0f);
+        }
+
+
+        public void Close()
+        {
+            transform.SetParent(closedPoint, false);
+            imageTransform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+}
