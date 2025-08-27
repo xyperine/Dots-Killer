d4ac2c38c6389e5826e72dd8bb0da06b07c92c31
xyperine <ewsannne@gmail.com>
2025-08-27 14:00:15 +0300

    Added version text script

---
+++ Assets/Scripts/UI/VersionText.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DotsKiller.UI
+{
+    public class VersionText : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text text;
+        [SerializeField] private string format = "v{0}";
+
+
+        private void OnValidate()
+        {
+            UpdateText();
+        }
+
+
+        private void Start()
+        {
+            UpdateText();
+        }
+
+
+        private void UpdateText()
+        {
+            text.text = string.Format(format, Application.version);
+        }
+    }
+}
