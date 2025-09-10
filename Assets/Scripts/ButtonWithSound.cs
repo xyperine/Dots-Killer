d24163a52caf6c0f7d237d533c083397db486084
xyperine <ewsannne@gmail.com>
2025-09-11 01:26:23 +0300

    Added script to connect buttons with audio

---
+++ Assets/Scripts/ButtonWithSound.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Zenject;
+
+namespace DotsKiller
+{
+    public class ButtonWithSound : MonoBehaviour, IPointerEnterHandler
+    {
+        [SerializeField] private Button button;
+        
+        private AudioManager _audioManager;
+
+
+        [Inject]
+        public void Initialize(AudioManager audioManager)
+        {
+            _audioManager = audioManager;
+        }
+
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(PlayOnClick);
+        }
+
+
+        public void PlayOnClick()
+        {
+            _audioManager.PlaySound(AudioID.ButtonClick);
+        }
+
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            PlayOnHover();
+        }
+
+
+        public void PlayOnHover()
+        {
+            _audioManager.PlaySound(AudioID.ButtonHover);
+        }
+
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(PlayOnClick);
+        }
+    }
+}
