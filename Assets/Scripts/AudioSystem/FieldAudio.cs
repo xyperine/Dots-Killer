05c961434c5acc247d21b54dc53aa9ef146ff405
xyperine <ewsannne@gmail.com>
2025-09-11 15:08:16 +0300

    Connected audio to the field

---
+++ Assets/Scripts/AudioSystem/FieldAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace DotsKiller.AudioSystem
+{
+    public class FieldAudio : MonoBehaviour
+    {
+        private AudioManager _audioManager;
+        
+        
+        [Inject]
+        public void Initialize(AudioManager audioManager)
+        {
+            _audioManager = audioManager;
+        }
+
+
+        public void PlaySound()
+        {
+            _audioManager.PlaySound(AudioID.FieldClick);
+        }
+    }
+}
