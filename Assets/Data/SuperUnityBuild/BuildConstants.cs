b21eacda857dd72005fd6224d77b51333930333d
xyperine <ewsannne@gmail.com>
2025-09-15 20:59:19 +0300

    Added platform dependent material switch

--- Assets/Data/SuperUnityBuild/BuildConstants.cs
+++ Assets/Data/SuperUnityBuild/BuildConstants.cs
@@ -38,11 +38,11 @@ namespace SuperUnityBuild.Generated
 
     public static class BuildConstants
     {
-        public static readonly DateTime buildDate = new DateTime(638924479110245792);
-        public const string version = "0.0.1.9376:5327";
-        public const int buildCounter = 16;
+        public static readonly DateTime buildDate = new DateTime(638935658032344382);
+        public const string version = "0.0.1.b9389:4974";
+        public const int buildCounter = 23;
         public const ReleaseType releaseType = ReleaseType.Test;
-        public const Platform platform = Platform.Windows;
+        public const Platform platform = Platform.Android;
         public const ScriptingBackend scriptingBackend = ScriptingBackend.IL2CPP;
         public const Target target = Target.Player;
         public const Distribution distribution = Distribution.None;
---
+++ Assets/Scripts/PlatformBasedMaterialSwitch.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DotsKiller
+{
+    public class PlatformBasedMaterialSwitch : MonoBehaviour
+    {
+        [SerializeField] private Renderer renderer;
+        [SerializeField] private Material webGLMaterial;
+        [SerializeField] private Material pcMaterial;
+        [SerializeField] private Material mobileMaterial;
+
+
+        private void Awake()
+        {
+            Material material = default;
+            
+#if UNITY_STANDALONE
+            material = pcMaterial;
+#elif UNITY_WEBGL
+            material = webGLMaterial;
+#elif UNITY_ANDROID
+            material = mobileMaterial;
+#elif UNITY_EDITOR
+            if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneWindows64)
+            {
+                material = pcMaterial;
+            }
+            else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.WebGL)
+            {
+                material = webGLMaterial;
+            }
+            else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android)
+            {
+                material = mobileMaterial;
+            }
+#else
+            material = renderer.material;
+#endif
+
+            renderer.material = material;
+        }
+    }
+}
